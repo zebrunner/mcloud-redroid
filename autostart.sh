75fa157bc28a905fe8b4d89e6ee4528266d4de2a
azarouski <azarouski@solvd.com>
2023-10-05 16:15:58 +0200

    autostart.sh was edited

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 cd /opt/zebrunner/mcloud-redroid/
 
 if [[ -f backup/settings.env ]] && [[ -f .env ]]; then
-  ./zebrunner.sh restart
+  ./zebrunner.sh start
 else
   ./zebrunner.sh start < backup/setup.txt
 fi
