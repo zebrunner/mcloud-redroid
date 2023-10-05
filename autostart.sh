e0cea3d06e399a9dc7804b714a824abdac026ad7
azarouski <azarouski@solvd.com>
2023-10-05 18:55:27 +0200

    autostart.sh was removed

--- autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /opt/zebrunner/mcloud-redroid/
-
-if [[ -f backup/settings.env ]] && [[ -f .env ]]; then
-  ./zebrunner.sh start
-else
-  ./zebrunner.sh start < backup/setup.txt
-fi
